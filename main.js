900e57b4ad858f5382e634aabb82b33d610090bd
Cuong Nguyen <cuongnmph04158@gmail.com>
2019-09-13 11:30:18 +0700

    update index

--- main.js
+++ main.js
@@ -130,7 +130,9 @@ let Game = function () {
     this._color = '#3498db';
     this._selectArray = [];
     this.time = TIME;
-    var self = this;
+    this._number5050 = 0;
+    this._keep = 0;
+    let self = this;
 
 
     this.startGame = function () {
@@ -155,6 +157,7 @@ let Game = function () {
         $("#question-number").html(this.level + 1);
         $("#display-question").html(QUESTION[this.level]);
         $("#reward").empty();
+        $("#surveyBoard").empty();
         this.show($('#board-button'));
         this.hide($("#submit-answer"));
         this.hide($('#restart-game'));
@@ -281,6 +284,17 @@ let Game = function () {
         self.congrats();
         self.withdraw();
     })
+
+    this.getElement('helpSurvey-button').addEventListener('click',function(){
+        self.helpSurvey();
+        self.showSurvey();
+    })
+    this.getElement('help5050-button').addEventListener('click',function(){
+        self.help5050();
+
+        //self.showSurvey();
+    })
+
 }
 Game.prototype.hide = function (elementDom) {
     this._elementDom = elementDom;
@@ -321,6 +335,60 @@ Game.prototype.drawMap = function () {
 
 };
 
+Game.prototype.drawChart = function (valueA, valueB, valueC, valueD) {
+            this.hide($("#reward"));
+            this.show($("#survey-region"));
+            var i = 0;
+            $("#chartA").append("<span class='ABCD'>A</span><span class='startPercent'></span>");
+            $("#chartB").append("<span class='ABCD'>B</span><span class='startPercent'></span>");
+            $("#chartC").append("<span class='ABCD'>C</span><span class='startPercent'></span>");
+            $("#chartD").append("<span class='ABCD'>D</span><span class='startPercent'></span>");
+
+            for (i = 1; i <= valueA; i++) {
+                setTimeout(function () {
+                    $("#chartA").append("<span class='percent'></span>")
+                }, 100)
+            }
+            for (i = 1; i <= valueB; i++) {
+                setTimeout(function () {
+                    $("#chartB").append("<span class='percent'></span>")
+
+                }, 100)
+            }
+            for (i = 1; i <= valueC; i++) {
+                setTimeout(function () {
+                    $("#chartC").append("<span class='percent'></span>")
+
+                }, 100)
+            }
+            for (i = 1; i <= valueD; i++) {
+                setTimeout(function () {
+                    $("#chartD").append("<span class='percent'></span>")
+
+                }, 100)
+            }
+
+            setTimeout(function () {
+                $("#chartA").append("<span class='percentNumber'> " + valueA + "%</span>")
+
+            }, 900);
+
+            setTimeout(function () {
+                $("#chartB").append("<span class='percentNumber'> " + valueB + "%</span>")
+
+            }, 900);
+
+            setTimeout(function () {
+                $("#chartC").append("<span class='percentNumber'> " + valueC + "%</span>")
+
+            }, 900);
+
+            setTimeout(function () {
+                $("#chartD").append("<span class='percentNumber'> " + valueD + "%</span>")
+
+            }, 900)
+
+        };
 Game.prototype.changeStepColor = function () {
     this.point += QUESTION[this.level].point;
     var currentStep = this.level + 1;
@@ -420,6 +488,174 @@ Game.prototype.resetBoardStatus = function () {
     clearInterval(this._interval);
 
 };
+Game.prototype.help5050 = function () {
+            var answerBox = this.getCorrect();
+            var random1 = this.random(1, 3);
+            var random2 = this.random(1, 3);
+            this.boxWrong(answerBox, random1, random2);
+            this._use5050 = true;
+            this._number5050 = this.level + 1;
+            //console.log(self._keep);
+            //console.log(answerBox);
+};
+Game.prototype.NumberBoxNameChanger = function (value) {
+            //console.log(value);
+            if (value == "A") {
+                return 1;
+            } else if (value == "B") {
+                return 2;
+            } else if (value == "C") {
+                return 3;
+            } else if (value == "D") {
+                return 4;
+            } else if (value == 1) {
+                return "boxA"
+            } else if (value == 2) {
+                return "boxB"
+            } else if (value == 3) {
+                return "boxC"
+            } else {
+                return "boxD"
+            }
+
+};
+Game.prototype.boxWrong = function (boxName, random1, random2) {
+            var boxID = this.NumberBoxNameChanger(boxName);
+            console.log(boxID);
+            if (random1 == random2) {
+                if (random1 !== 1) {
+                    random1--
+                } else {
+                    random1++
+                }
+            }
+
+            var number1 = (boxID + random1) % 4;
+            if (number1 == 0)(number1 = 4);
+            var number2 = (boxID + random2) % 4;
+            if (number2 == 0)(number2 = 4);
+            var boxWrong1 = this.NumberBoxNameChanger(number1);
+            var boxWrong2 = this.NumberBoxNameChanger(number2);
+            //console.log(boxWrong1,boxWrong2);
+            for (var i = 1; i <= 4; i++) {
+                if (i !== number1 && i !== number2 && i !== boxID) self._keep = i;
+            }
+
+
+            this.clearWrong(boxWrong1, boxWrong2);
+
+        };
+
+Game.prototype.clearWrong = function (boxName1, boxName2) {
+
+            boxName1 = "#" + boxName1;
+            boxName2 = "#" + boxName2;
+            $(boxName1).attr('class', 'boxWrong');
+            $(boxName2).attr('class', 'boxWrong');
+            $(boxName1).css('pointer-events', 'none');
+            $(boxName2).css('pointer-events', 'none');
+            $(boxName1).css('background-color', '#3498db');
+            $(boxName2).css('background-color', '#3498db');
+
+};
+Game.prototype.helpSurvey = function () {
+            var answerBox = this.getCorrect();
+            //console.log(answerBox);
+            var sum = 76;
+            var value1 = this.random(0, sum);
+            var value2 = this.random(0, sum - value1);
+            var value3 = this.random(0, sum - value1 - value2);
+            var value4 = sum - value1 - value2 - value3;
+            var valueA = this.random(1, 4);
+            var valueB = 0;
+            var valueC = 0;
+            var valueD = 0;
+
+
+            if (valueA === 1) {
+                valueA = value1;
+                valueB = value2;
+                valueC = value3;
+                valueD = value4;
+            } else if (valueA === 2) {
+                valueA = value2;
+                valueB = value3;
+                valueC = value4;
+                valueD = value1;
+            } else if (valueA === 3) {
+                valueA = value3;
+                valueB = value4;
+                valueC = value1;
+                valueD = value2;
+            } else {
+                valueA = value4;
+                valueB = value1;
+                valueC = value2;
+                valueD = value3;
+            }
+
+
+            if (answerBox === "boxA") {
+                valueA = valueA + 24;
+
+            } else if (answerBox === "boxB") {
+                valueB = valueB + 24;
+
+            } else if (answerBox === "boxC") {
+                valueC = valueC + 24;
+            } else {
+                valueD = valueD + 24;
+            }
+
+
+            if (this._number5050 == this.level + 1) {
+
+                var sumRemove = 0;
+
+
+                if (self._keep !== 1 || answerBox !== "boxA") {
+
+                    sumRemove += valueA;
+                    valueA = 0;
+                }
+                if (self._keep !== 2 && answerBox !== "boxB") {
+                    sumRemove += valueB;
+                    valueB = 0;
+
+                }
+                if (self._keep !== 3 && answerBox !== "boxC") {
+                    sumRemove += valueC;
+                    valueC = 0;
+
+                }
+                if (self._keep !== 4 && answerBox !== "boxD") {
+                    sumRemove += valueD;
+                    valueD = 0;
+
+                }
+
+
+                if (answerBox === "boxA") {
+                    valueA = valueA + sumRemove;
+
+                } else if (answerBox === "boxB") {
+                    valueB = valueB + sumRemove;
+
+                } else if (answerBox === "boxC") {
+                    valueC = valueC + sumRemove;
+                } else {
+                    valueD = valueD + sumRemove;
+                }
+
+
+            }
+
+            this.drawChart(valueA, valueB, valueC, valueD);
+            this._useSurvey = true;
+
+
+};       
+
 Game.prototype.changeSubmitAnswerButton = function () {
     this.checkAnswer() ?  this.show($("#next-question")): this.show($("#restart-game"));
 
@@ -465,7 +701,11 @@ Game.prototype.nextOrStop = function () {
     this.hide($("#submit-answer"))
 
 };
+Game.prototype.random = function (min, max) {
+    //this._use5050 = true;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 
+};
 
 
 
